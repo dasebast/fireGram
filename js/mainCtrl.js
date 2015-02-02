506cd1cf75e050db664ef76b7f01ba7034d1b7fa
Doug Sebastian <dasebast@gmail.com>
2015-02-02 11:37:25 -0700

    adding initial files

---
+++ js/mainCtrl.js
@@ -0,0 +1,7 @@
+var app = angular.module('fireGram');
+
+app.controller('mainCtrl', function($scope, $firebase) {
+
+	$scope.test = "hi";
+
+});
---
+++ js/mainService.js
@@ -0,0 +1,15 @@
+var app = angular.module('fireGram');
+
+app.service('mainService', function($firebase) {
+
+var ref = new Firebase('https://firegram.firebaseio.com/');
+
+	
+
+
+
+	
+
+
+
+});
